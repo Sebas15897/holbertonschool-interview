aa66ceeb4db2af1b35c19f7d77489acf239e095f
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-08-15 14:03:49 -0500

    Update 0-minoperations.py

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,38 +1,20 @@
 #!/usr/bin/python3
-"""
-calculates the fewest number of operations needed to result
-in exactly n H characters in the file.
-"""
+""" Minimum Operations """
 
 
 def minOperations(n):
-    """
-    minOperations min operations
-    Args:
-        n :int
-    """
-    t = n
-    i2 = 0
-    i3 = 0
-    i5 = 0
-    i7 = 0
-    i11 = 0
-    if (n <= 0):
+    """ function that's calculate minium operations"""
+    if not isinstance(n, int) or n < 2:
         return 0
-    while (t % 2 == 0):
-        i2 += 1
-        t = t / 2
-    while (t % 3 == 0):
-        t = t / 3
-        i3 += 1
-    while (t % 5 == 0):
-        t = t/5
-        i5 += 1
-    while (t % 7 == 0):
-        t = t / 7
-        i7 += 1
-    while (t % 11 == 0):
-        t = t / 11
-        i11 += 1
-    m = i5 * 5 + i3 * 3 + i2 * 2 + i7 * 7 + i11 * 11
-    return m
+    operation = 0
+    odd = 3
+
+    while n % 2 == 0:
+        operation += 2
+        n = n//2
+    while odd <= n:
+        while n % odd == 0:
+            operation += odd
+            n = n//odd
+        odd += 2
+    return int(operation)
