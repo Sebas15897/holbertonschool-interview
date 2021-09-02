4f1aacd9b3d545270851b5892842a6410bf927b8
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-09-02 10:20:08 -0500

    Update 0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,59 +1,50 @@
 #!/usr/bin/python3
-
+""" Write a script that reads stdin line by line and
+computes metrics
+Input format: <IP Address> - [<date>]
+"GET /projects/260 HTTP/1.1"
+<status code> <file size>
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning:
+Total file size: File size: <total size>
+where <total size> is the sum of all previous <file size>
+(see input format above)
+Number of lines by status code:
+possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+if a status code doesn’t appear, don’t print anything
+for this status code
+format: <status code>: <number>
+status codes should be printed in ascending order
 """
-Log Parser
-"""
-
-
+import re
 import sys
 
 
-total_file_size = {'size': 0}
-codes = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
+def print_sorted_dict(status_codes):
+    """
+    status_code have all data args with codes and times
+    """
+    sorted_keys = sorted(status_codes.keys())
+    print('\n'.join(["{:d}: {:d}".format(k, status_codes[k])
+                     for k in sorted_keys if status_codes[k] != 0]))
 
 
-def print_data():
-    """
-        Prints the resume
-    """
-    print('File size: {}'.format(total_file_size['size']))
-    for key in sorted(codes.keys()):
-        if codes[key] > 0:
-            print('{}: {}'.format(key, codes[key]))
-
-
-def operate_resume(line):
-    """
-        Operates with the resume stats
-    """
+def __run():
     try:
-        line = line.split(' ')
-        size = line[-1]
-        total_file_size['size'] += int(size)
-        if line[-2] in codes:
-            codes[line[-2]] += 1
-    except Exception as e:
-        pass
+        total = 0
+        status_codes = \
+            {code: 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
+        for i, rawline in enumerate(sys.stdin, 1):
+            words = rawline.split()
+            total += int(words[-1])
+            status_codes[int(words[-2])] += 1
+            if i % 10 == 0:
+                print("File size: {:d}".format(total))
+                print_sorted_dict(status_codes)
+    finally:
+        print("File size: {:d}".format(total))
+        print_sorted_dict(status_codes)
 
 
-if __name__ == '__main__':
-    num_lines = 1
-    try:
-        for line in sys.stdin:
-            operate_resume(line)
-            if num_lines % 10 == 0:
-                print_data()
-            num_lines += 1
-    except KeyboardInterrupt:
-        print_data()
-        raise
-    print_data()
+if __name__ == "__main__":
+    __run()
