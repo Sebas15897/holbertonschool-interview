07ee53d829b9c55a12f6d28cdc1832337cf87560
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2022-03-19 12:53:19 -0500

    Update 0-nqueens.py

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,80 +1,57 @@
 #!/usr/bin/python3
-""" The N queens puzzle is the challenge of placing N
-     non-attacking queens on an N×N chessboard.
-     Write a program that solves the N queens problem."""
+"""
+N queens algorithm
+"""
 
 import sys
-
 
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
-if not sys.argv[1].isdigit():
+
+try:
+    nQueen = int(sys.argv[1])
+except ValueError:
     print("N must be a number")
     exit(1)
-if int(sys.argv[1]) < 4:
+
+if nQueen < 4:
     print("N must be at least 4")
     exit(1)
-N = int(sys.argv[1])
-board = [[0 for i in range(N)]for j in range(N)]
-k = 1
 
 
-def print_sol(board):
-    """ Method Print"""
-    global k
-    k = k + 1
-    S = []
-    for i in range(N):
-        for j in range(N):
-            if board[i][j] == 1:
-                S.append([i, j])
-    print(S)
+def solve_nqueens(n):
+    """ self descriptive
+    """
+    if n == 0:
+        return [[]]
+    inner_solution = solve_nqueens(n - 1)
+    return [solution + [(n, i + 1)]
+            for i in range(nQueen)
+            for solution in inner_solution
+            if safe_queen((n, i + 1), solution)]
 
 
-def isSafe(board, row, col):
-    """ Method IsSafe """
-    for i in range(col):
-        if (board[row][i]):
+def attack_queen(square, queen):
+    """ self descriptive
+    """
+    (row1, col1) = square
+    (row2, col2) = queen
+    return (row1 == row2) or (col1 == col2) or \
+        abs(row1 - row2) == abs(col1 - col2)
+
+
+def safe_queen(sqr, queens):
+    """ self descriptive
+    """
+    for queen in queens:
+        if attack_queen(sqr, queen):
             return False
-    i = row
-    j = col
-    while i >= 0 and j >= 0:
-        if(board[i][j]):
-            return False
-        i -= 1
-        j -= 1
-    i = row
-    j = col
-    while j >= 0 and i < N:
-        if(board[i][j]):
-            return False
-        i = i + 1
-        j = j - 1
     return True
 
 
-def solving(board, col):
-    """ Method Solving """
-    if (col == N):
-        print_sol(board)
-        return True
-    res = False
-    for i in range(N):
-        if (isSafe(board, i, col)):
-            board[i][col] = 1
-            res = solving(board, col + 1) or res
-            board[i][col] = 0
-    return res
-
-
-def solve():
-    """ Method Solve"""
-    board = [[0 for j in range(N)]for i in range(N)]
-    if (solving(board, 0) is False):
-        print("Solution does not exist")
-        return
-    return
-
-
-solve()
+for answer in reversed(solve_nqueens(nQueen)):
+    result = []
+    for p in [list(p) for p in answer]:
+        result.append([i - 1 for i in p])
+    print(result)
