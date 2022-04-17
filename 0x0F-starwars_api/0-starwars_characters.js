dad0ae06af3205c0059e49b8a478a17c73a1378d
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2022-04-17 14:41:25 -0500

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,2 +1,25 @@
-#!/bin/bash
-python3 -m compileall -b $PYFILE
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+request(url, async function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(character, (error, res, html) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
+      });
+      console.log(res);
+    }
+  }
+});
